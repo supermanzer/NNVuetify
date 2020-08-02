aca33a67ea5508ff8f4644320ba34ec4e68273bd
Ryan <ryan.manzer@gmail.com>
2020-08-02 11:40:24 -0700

    Buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="display-4">Home Page</h1>
+    <v-btn class="pink white--text" :elevation="4">Click Me</v-btn>
+    <p></p>
+    <v-btn dark depressed color="deep-purple">Click Me</v-btn>
+    <p></p>
+    <v-btn text color="pink">Click Me</v-btn>
+    <p></p>
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-comment</v-icon>
+      <span>Comment</span>
+    </v-btn>
+    <p></p>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-comment</v-icon>
+      <span>Comment</span>
+    </v-btn>
+    <p></p>
+    <v-btn depressed large class="pink white--text">
+      <span>Comment</span>
+      <v-icon right large>mdi-comment</v-icon>
+    </v-btn>
+    <p></p>
+    <v-btn depressed small dark fab color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
